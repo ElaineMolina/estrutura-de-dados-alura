86421ec989ff2533304b9334e615ae722efcbf57
ElaineMolina <elainemolina10@gmail.com>
2021-11-24 09:29:21 -0300

    commit inicial

---
+++ src/Vetor.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class Vetor {
+	private Aluno[] alunos = new Aluno[100];
+	private int totalDeAlunos = 0;
+
+	public void adiciona(Aluno aluno) { // dessa maneira leva tempo constante
+		this.alunos[totalDeAlunos] = aluno;
+		totalDeAlunos++;
+	}
+
+	public void adiciona(int posicao, Aluno aluno) {
+		for(int i = totalDeAlunos - 1; i >= posicao; i -= 1 ) {
+			alunos[i+1] = alunos[i];
+		}
+		alunos[posicao] = aluno;
+		totalDeAlunos++;
+	}
+
+	private boolean posicaoOcupada(int posicao) {
+		return posicao >= 0 && posicao < totalDeAlunos;
+	}
+
+	public Aluno pega(int posicao) {
+
+		if (!posicaoOcupada(posicao)) {
+			throw new IllegalArgumentException("posição inválida");
+		}
+		return alunos[posicao];
+	}
+
+	public void remove(int posicao) {
+
+	}
+
+	public boolean contem(Aluno aluno) {
+		for (int i = 0; i < totalDeAlunos; i++) {
+			if (aluno.equals(alunos[i])) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int tamanho() {
+		return totalDeAlunos;
+	}
+
+	public String toString() {
+		return Arrays.toString(alunos);
+	}
+}
